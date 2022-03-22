c9767a32c5a9978676a06fd2678d183f2c54c4ce
developing-stevie <mitchellevie@icloud.com>
2022-03-22 00:21:47 -0400

    week 5 turned in (needs work)

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -26,6 +26,60 @@ const secretCodeWord3 = "Eccentric"
 // b) Create the function that makes the test pass.
 
 
+// a describe method that lists the name of the function OR naming of the particular test.
+describe("code", () => {
+
+    // a test/it method, nested within the describe block, that in plain words, describes that the function does.
+    it("that takes in a string and returns a coded message. The coded message converts 'a' to 4, 'e' to 3, 'i' to 1, and 'o' to 0.", () => {
+  
+      //an expect method, nested within the test block, calling on the hello() function, followed by the .toEqual() matcher that checks the expected output of the function return.
+      expect(code(secretCodeWord1)).toEqual("L4ck4d41s1c4l")
+      expect(code(secretCodeWord2)).toEqual("G0bbl3dyg00k")
+      expect(code(secretCodeWord3)).toEqual("3cc3ntr1c")
+    })
+  })
+
+
+//   //create a function that if needed converts the string to an array
+// //then splits it 
+// //then searches the indexs values for the the arguments above
+// //and either replaces them or converts them to the numerals desired 
+// //to achive the desired result outputing coded messages - replace method may help but may also be too vauge vs a regular math equation using methods
+
+
+
+
+ const code = (string) => {
+    string = string.replace(/a/g,'4').replace(/A/g,'4').replace(/i/g,'1').replace(/I/g,'1').replace(/e/g,'3').replace(/E/g,'3').replace(/o/g,'0').   replace(/O/g,'0')
+       
+    return string
+ }
+
+
+//      Test Suites: 1 passed, 1 total
+// Tests:       1 passed, 1 total
+
+
+
+
+
+
+
+//------------------------------------old code and snips
+// if (string.inculudes('a', 'A')) {
+//     return string.replace('aA', '4')
+
+// }else{ (string.inculudes('e', 'E'))
+//     return string.replace('eE', '3')
+
+// }else{ (string.inculudes('i', 'I'))
+//     return string.replace('iI', '1')
+// }if{
+//     return string
+
+
+
+
 // --------------------2) Create a function that takes in an array of words and a single letter and returns all the words that contain that particular letter.
 
 // a) Create a test with expects statement using the variable provided.
@@ -41,6 +95,39 @@ const letterE = "e"
 // b) Create the function that makes the test pass.
 
 
+
+// a describe method that lists the name of the function OR naming of the particular test.
+describe("newArr", () => {
+
+    // a test/it method, nested within the describe block, that in plain words, describes that the function does.
+    it("that takes in an array of words and a single letter and returns all the words that contain that particular letter.", () => {
+  
+      //an expect method, nested within the test block, calling on the hello() function, followed by the .toEqual() matcher that checks the expected output of the function return.
+      expect(newArr(arrayOfWords1,letterA)).toEqual(["Apple", "Banana", "Orange"])
+      expect(newArr(arrayOfWords2,letterE)).toEqual(["Cherry", "Blueberry", "Peach"])
+    })
+  })
+
+
+  //use .lowercase to making all letters the same
+//filter words including a and e
+//constant newArr equals (array, letter)
+// to return filtered array value
+//.lowercase all values if incluedes letter stictly equals true
+
+
+
+const newArr = (array, letter) => {
+  return array.filter(word => word.toLowerCase().includes(letter))
+}
+
+
+// Test Suites: 1 passed, 1 total
+// Tests:       2 passed, 2 total
+// Snapshots:   0 total
+// Time:        1.617 s, estimated 2 s
+
+
 // --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a “full house”. A full house is exactly one pair and one three of a kind.
 
 // a) Create a test with expect statements using the variable provided.
@@ -54,3 +141,59 @@ const hand3 = [5, 5, 5, 5, 4]
 
 
 // b) Create the function that makes the test pass.
+
+
+
+// a describe method that lists the name of the function OR naming of the particular test.
+describe('numb', () => {
+
+    // a test/it method, nested within the describe block, that in plain words, describes that the function does.
+    it("that takes in an array of 5 numbers and determines whether or not the array is a full house", () => {
+  
+      //an expect method, nested within the test block, calling on the hello() function, followed by the .toEqual() matcher that checks the expected output of the function return.
+      expect(numb(hand1)).toEqual(true)
+      expect(numb(hand2)).toEqual(false)
+      expect(numb(hand3)).toEqual(false)
+    })
+  })
+
+
+
+//use sort to order the array if needed
+//use a funtion method or method to iterate through the 
+//if A = B = c 
+//then return true
+// if d = e but not to A,B,C
+//then return string "[ ] is a full house"
+//else return string "[ ] is not a full house"
+//instead of say index 0 is the same as index 2
+
+
+//  x x x y y
+// [0,1,2,3,4]
+// not sure why its working i think it has to do with my test of syntax
+
+
+const numb = (array) => {
+let special = 0
+if ([...new Set(array)].length === 2 && array.length === 5){
+
+}
+
+}
+
+
+// Test Suites: 1 failed, 1 total
+// Tests:       1 failed, 2 passed, 3 total
+
+// const fullHouse = [array] => {
+//     array.sort((a,b) => a-b);
+//     if(array[0] === array[1] && array[2] === array[4] && array[0] !== array[4]){
+//         return "[] is a full house"
+//     }
+// else if(array[0] === array[2] && array[3] === array[4] && array[0] !== array[4]{
+//     return "[] is a full house"
+// }
+// else 
+// return "not a full house"
+// }
--- ruby-challenges.rb
+++ ruby-challenges.rb
@@ -7,8 +7,36 @@ beverages_array = ['coffee', 'tea', 'juice', 'water', 'soda water']
 
 letter_o = 'o'
 # Expected output: ['coffee', 'soda water']
-letter_t = 't'
+letter_t = 't' 
 # Expected output: ['tea', 'water', 'soda water']
+
+# use include method to check index
+# if index inculdes o or t
+# output new array with index values include the letters
+# const newArr = (array, letter) => {
+#   return array.filter(word => word.toLowerCase().includes(letter))
+# }
+#not sure why its returning undefined 
+
+# def new_array (array, letter) 
+#     array.select do |value|
+#         if value.inculde? letter
+#         end
+#     end
+
+
+# def new_array (array, letter) 
+#     list = []
+#  array.map do |value|
+#    if value.inculde? letter
+# list.push(value)
+#    end
+# end
+# return list
+# end
+# p (new_array(beverages_array,letter_t))
+
+
 
 
 # -------------------2) Create a method that takes in an array of numbers and returns the sum of the numbers. Use the test variables provided.
@@ -19,12 +47,73 @@ nums_array1 = [42, 7, 27]
 nums_array2 = [25, 17, 47, 11]
 # Expected output: 100
 
+# use .sum method to get output sum of array
+# or .inject
+# use the code to add each index to each other using .each
+# keep getting unexpected end of input not sure why 
+
+def numbers = array
+
+def sum(numbers)
+    return 0 if numbers.length < 1
+    result = 0
+    numbers.each { |num| results += num }
+    result
+end
+
+end 
+
+p sum
+
 
 
 # --------------------3a) Create a class called Bike that is initialized with a model, wheels, and current_speed. The default number of wheels is 2. The current_speed should start at 0. Create a bike_info method that returns a sentence with all the data from the bike object.
 
 # Expected output example: 'The Trek bike has 2 wheels and is going 0 mph.'
 
+
+#------------------------check list 
+ #If there is not a checkmark, then please reconsider how you are approaching that part of your method. If a value is an integer, it will not need quotations.
+# Make a class. :white_check_mark:
+# Setter and getter for variables as symbols. attr_accessor :white_check_mark:
+# Initialize method  and pass each parameter that doesn't have a default value.
+# model does not have a default value :white_check_mark:
+# wheels does
+# current_speed does
+# Name a method that outputs values in a sentence :white_check_mark:
+# String interpolation :white_check_mark:
+# Make a new instance with the expected argument(s)
+# Call the method on this new instance
+# Let me know if that helps for the first part of the question 
+# Second part.
+# You will add a couple of methods to your Bike class.
+# A method that will add value to speed up
+# A method that will subtract value
+# You will also call these methods on your new instance
+
+
+#----------------------------------psudo
+#named class bike 
+#add accessor current speed
+#initialize model wheels with minumum of 2 current speed set to 0
+#define bike info with information string
+#unsure how to fully complete this got stuck on adding pedel option i'll read on classes in ruby more and request assistence on the topic
+
+# class Bike
+# attr_accessor :current_speed
+# def initialize(model, wheels=2, current_speed=0) 
+#     @model = model
+#     @wheels = wheels
+#     @current_speed = current_speed
+#  end
+
+#  def bike_info
+#     "The #{@model} has #{@wheels} and is going #{@current_speed} mph."
+#  end
+
+#  tricycle = Bike.new('Tricycle')
+#  tricycle.bike_info
+#  expected output example: 'the Trek bike has 2 wheels and is going 0 mph'
 
 
 # -------------------3b) Add the ability to pedal faster and brake. The pedal_faster method should increase the speed by a given amount. The brake method should decrease the speed by a given amount. The bike cannot go negative speeds.
@@ -33,3 +122,8 @@ nums_array2 = [25, 17, 47, 11]
 # Expected output example: my_bike.pedal_faster(18) => 28
 # Expected output example: my_bike.brake(5) => 23
 # Expected output example: my_bike.brake(25) => 0
+
+
+
+
+
